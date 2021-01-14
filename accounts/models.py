d6d385070ec3cea32e799de25aafaca45ee2a86f
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-01-14 16:50:45 +0400

    slugs added

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from .common import slugify
 
 
 class User(AbstractUser):
     terms_agreement = models.BooleanField(default=False)
     email = models.EmailField(('email adress'), unique=True, null=True)
     user_type = models.ManyToManyField('UserType',  verbose_name=("User Type"))
+    slug = models.SlugField(max_length=255, null=True, blank=True)
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        self.slug = f'{slugify(self.username)}'
+        super(User, self).save(*args, **kwargs)
 
 class UserType(models.Model):
     title = models.CharField(max_length=80)

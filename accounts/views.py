e7474ddbeeadb86ee467079a597109a576f3681f
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-01-14 15:34:18 +0400

    sending mail on register added

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from accounts.models import User
 from .forms import RegisterForm, LoginForm, ResetItDown, PasswordResetConfirmForm
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView, PasswordChangeView
 from django.core.exceptions import PermissionDenied
+from django.core.mail import send_mail
 
 
 class RegisterView(CreateView):
@@ -12,6 +13,15 @@ class RegisterView(CreateView):
     template_name = 'register.html'
     form_class = RegisterForm
     success_url = reverse_lazy('core:congrats-page')
+
+    def form_valid(self, form):
+        user_email = form.instance.email
+        send_mail('subject', 'body of the message', 'tech.academy.user2@gmail.com', [user_email,])
+        # form.instance.is_sell = False
+        # form.instance.product = self.get_object()
+        # print(form.instance.price)
+        form.save()
+        return super().form_valid(form)
     
 class LoginUserView(LoginView):
     template_name = 'login.html'

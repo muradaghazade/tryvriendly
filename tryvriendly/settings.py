9b27da32d9ef94e48f2c985a7efec07c653d6818
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-01-18 12:19:36 +0400

    dockerezation

--- tryvriendly/settings.py
+++ tryvriendly/settings.py
@@ -88,9 +88,17 @@ WSGI_APPLICATION = 'tryvriendly.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'compass',
+        'USER': 'muradaghazada',
+        'PASSWORD': '885522',
+        'PORT': '5432',
+        'HOST': '127.0.0.1'
     }
 }
 

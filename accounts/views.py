e647268e7ef59e47d97f7e8c9685b2876c696fa5
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-01-22 11:37:37 +0400

    backend

--- accounts/views.py
+++ accounts/views.py
@@ -17,16 +17,6 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from knox.models import AuthToken
-from .serializers import UserSerializer, RegisterSerializer
-
-from django.contrib.auth import login
-
-from rest_framework import permissions
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
 
 def usersignup(request):
     if request.method == 'POST':
@@ -101,25 +91,3 @@ class EditProfileView(UpdateView):
         if self.request.user.username != self.get_object().username:
             raise PermissionDenied
         return super(EditProfileView, self).dispatch(request, *args, **kwargs)
-
-class RegisterAPI(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
-        })
-
-class LoginAPI(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
